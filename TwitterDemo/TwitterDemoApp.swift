8b0ee23a5e0c7e03fa33935f69e886e2ae73f209
Qwertu-Hacker <109757033+Qwertu-Hacker@users.noreply.github.com>
2022-07-23 13:55:20 +0300

    Initial Commit

---
+++ TwitterDemo/TwitterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterDemoApp.swift
+//  TwitterDemo
+//
+//  Created by Igor Kononov on 23.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
